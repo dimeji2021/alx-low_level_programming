9aa27a5a3284e58ebc518d0438eef8c9c843f196
Kehinde Aregbesola <adedimeji88@yahoo.com>
2023-02-17 08:09:31 -0800

    .

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,25 +10,24 @@
 
 int main(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i; j < 10; j++)
+for (i = 0; i < 10; i++)
+{
+	for (j = i; j < 10; j++)
 	{
-            if (i != j)
-	    {
-                putchar(i + '0');
-                putchar(j + '0');
-                if (i != 8 || j != 9)
+		if (i != j)
 		{
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(', ');
+			}
+		}
+	}
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
