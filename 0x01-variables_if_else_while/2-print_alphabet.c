984363974e450d876877d257b02de27361f65ed6
Kehinde Aregbesola <adedimeji88@yahoo.com>
2023-02-17 04:39:00 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+         putchar('\n');
+    }
+
+
+    return (0);
+}
