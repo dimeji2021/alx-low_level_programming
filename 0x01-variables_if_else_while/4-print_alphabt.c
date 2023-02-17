4a77d2472ac6ff9598219b6c278a9302f9cefb65
Kehinde Aregbesola <adedimeji88@yahoo.com>
2023-02-17 04:51:23 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if(letter == 'a' || letter == 'e') continue;
+        putchar(letter);
+        putchar('\n');
+    }
+
+    return (0);
+}
