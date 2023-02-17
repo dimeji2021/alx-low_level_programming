735aeaabb26c9641179f210c13aa6aadc1e74b02
Kehinde Aregbesola <adedimeji88@yahoo.com>
2023-02-17 07:22:57 -0800

    .

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,16 +9,15 @@
  */
 int main(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter == 'a' || letter == 'e')
-            continue;
-        putchar(letter);
-        putchar('\n');
-    }
-
-    return (0);
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter == 'a' || letter == 'e')
+	continue;
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
 
