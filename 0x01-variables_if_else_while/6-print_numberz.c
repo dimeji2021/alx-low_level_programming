dac60de07070d1316626e34ce3519de65cdc5420
Kehinde Aregbesola <adedimeji88@yahoo.com>
2023-02-17 07:46:23 -0800

    .

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,15 +9,13 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        int digit = i + '0'; // convert the integer to its ASCII character representation
-        putchar(digit);
-        putchar('\n');
-    }
-
-    return 0;
+int i;
+for (i = 0; i < 10; i++)
+{
+	int digit = i + '0';
+	putchar(digit);
+}
+putchar('\n');
+return (0);
 }
 
